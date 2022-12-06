24e7b7e7fa5a83613e85a01e47401d49bca9688c
dosorio55 <dosorio55@gmail.com>
2022-12-06 21:36:46 +0100

    trying users

--- server/api/models/user.model.js
+++ server/api/models/user.model.js
@@ -6,6 +6,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     password: { type: String, required: true },
     image: { type: String, required: true },
+    secretSanta: { type: String }
   },
   {
     timestamps: true,
